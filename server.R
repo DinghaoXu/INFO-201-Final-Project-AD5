69dd1b082cf046e4ae82caa051a6bf6f4fa5f57f
Dinghao Xu <dinghx@uw.edu>
2018-05-26 19:56:08 -0700

    start the final project

---
+++ server.R
@@ -0,0 +1,13 @@
+# Setup
+library(shiny)
+library(shinythemes)
+library(dplyr)
+library(ggplot2)
+
+# Define the server
+server <- function(input, output) {
+  
+}
+
+# Call the server
+shinyServer(server)
---
+++ ui.R
@@ -0,0 +1,10 @@
+# Setup
+library(shiny)
+library(shinythemes)
+library(dplyr)
+library(ggplot2)
+
+# Define the ui
+
+# Call the ui
+shinyUI(ui)
