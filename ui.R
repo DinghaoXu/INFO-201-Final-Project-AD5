61b2466535564491755a9e6ffa097a2ce2da57b0
Dinghao Xu <dinghx@uw.edu>
2018-05-26 21:16:26 -0700

    provide basic layout

--- ui.R
+++ ui.R
@@ -5,6 +5,14 @@ library(dplyr)
 library(ggplot2)
 
 # Define the ui
-
+ui <- navbarPage(theme = shinytheme("readable"),
+  tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+  tabPanel("Introduction"
+           ),
+  tabPanel("Coin volatility"),
+  tabPanel("Twitter"),
+  tabPanel("Recommendation"),
+  tabPanel("Feedback")
+)
 # Call the ui
 shinyUI(ui)
