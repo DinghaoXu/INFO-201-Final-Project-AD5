895bc0dc03a1b900e29ea2ab043a37383238c7f6
Dayoung Cheong <dcheong@uw.edu>
2018-05-29 22:12:10 -0700

    twitter data

--- ui.R
+++ ui.R
@@ -3,16 +3,35 @@ library(shiny)
 library(shinythemes)
 library(dplyr)
 library(ggplot2)
+library(wordcloud)
+library("tm")
+library("SnowballC")
+library("RColorBrewer")
 
 # Define the ui
-ui <- navbarPage(theme = shinytheme("readable"),
+ui <- navbarPage(
+  theme = shinytheme("readable"),
   tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
-  tabPanel("Introduction"
-           ),
-  tabPanel("Coin volatility"),
-  tabPanel("Twitter"),
-  tabPanel("Recommendation"),
-  tabPanel("Feedback")
+  tabsetPanel(
+    tabPanel(
+      "Twitter",
+      titlePanel(
+        sidebarLayout(
+          sidebarPanel(
+            sliderInput(
+              "min_freq_slider",
+              label = h3("Minimum Frequency:"), min = 0,
+              max = 6, value = 2
+            )
+          ),
+          # Main panel which displays barplot
+          mainPanel(
+            plotOutput("plot", width = "100%")
+          )
+        )
+      )
+    )
+  )
 )
 # Call the ui
 shinyUI(ui)
